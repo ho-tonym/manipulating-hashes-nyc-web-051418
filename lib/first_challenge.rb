bf09ee4d503621be522da6429d05c9c951faf099
Tony Ho <ho.tonym@gmail.com>
2018-05-09 02:05:24 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -22,7 +22,6 @@ contacts.each do |person,data|
     if attribute == :favorite_icecream_flavors
       value.each {|flavor|
            contacts[person][attribute].delete_if {|x, y| y == "strawberry" }
-           binding.pry
         }
     end
   end
